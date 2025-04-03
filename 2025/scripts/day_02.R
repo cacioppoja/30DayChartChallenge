189d7f38fbde14475c77a6ab5d0599eb73d6b487
cacioppoja <cacioppoja@gmail.com>
2025-04-02 22:07:44 -0500

    Added data and source

--- 2025/scripts/day_02.R
+++ 2025/scripts/day_02.R
@@ -2,6 +2,20 @@
 # Program: 30DayChartChallenge,  Day 02 - Slope
 # Date Created: 04/02/2025
 # Author: Jessi Cacioppo
+# Data Source: https://www.nielsen.com/news-center/2024/womens-college-basketball-championship-draws-record-breaking-18-9-million-viewers/
 ########
 
+library(readr)
+library(tidyverse)
+library(janitor)
 
+wbb <- read_delim(file = "C:/USers/Jessica/Documents/R Projects/30DayChartChallenge/2025/data/ncaawbb_viewership.txt")
+glimpse(wbb)
+
+wbb <- clean_names(wbb)
+
+plot <- wbb %>% 
+  ggplot() +
+  geom_point(aes(x=year, y=total_viewers_p2, color=network_s))
+
+plot
